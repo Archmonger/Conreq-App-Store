fe409c62cd0da95d89d50a755c770f4c77dae51f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-10 14:13:28 -0700

    model cleanup

--- conreq_app_store/internal/app_store/models.py
+++ conreq_app_store/internal/app_store/models.py
@@ -37,6 +37,22 @@ class DescriptionTypes(models.TextChoices):
     MARKDOWN = "text/markdown", "Markdown (.md)"
 
 
+class LicenseTypes(models.TextChoices):
+    GPL3 = "GPL3", "GNU GPL v3"
+    MIT = "MIT", "MIT License"
+    APACHE2 = "APACHE2", "Apache License 2.0"
+    BSD2 = "BSD2", "BSD 2-Clause Simplified License"
+    BSD3 = "BSD3", "BSD 3-Clause Revised License"
+    ECLIPSE2 = "ECLIPSE2", "Eclipse Public License 2.0"
+    AGPL3 = "AGPL3", "GNU AGPL v3"
+    GPL2 = "GPL2", "GNU GPL v2"
+    LGPL2_1 = "LGPL2_1", "GNU LGPL v2.1"
+    LGPL3 = "LGPL3", "GNU LGPL v3"
+    MOZ2 = "MOZ2", "Mozilla Public License 2.0"
+    UNLICENSE = "UNLICENSE", "The Unlicense"
+    OTHER = "OTHER", "Other"
+
+
 class Category(models.Model):
     def __str__(self):
         return self.name
@@ -46,7 +62,8 @@ class Category(models.Model):
 
     # Basic Info
     name = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
+    short_description = models.CharField(max_length=100, blank=True, null=True)
+    long_description = models.TextField(blank=True, null=True)
 
     # Tracker
     tracker = FieldTracker()
@@ -62,7 +79,8 @@ class Subcategory(models.Model):
 
     # Basic Info
     name = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
+    short_description = models.CharField(max_length=100, blank=True, null=True)
+    long_description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     # Tracker
@@ -79,7 +97,7 @@ class AppPackage(models.Model):
     # Basic Info
     package_name = models.CharField(max_length=100, help_text="Must be snake_case.")
     verbose_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
     long_description = models.FileField(upload_to="app_store/readme/")
     long_description_type = models.CharField(
         max_length=20,
@@ -88,36 +106,46 @@ class AppPackage(models.Model):
     )
     subcategories = models.ManyToManyField(Subcategory)
     development_stage = models.CharField(
-        max_length=21, choices=DevelopmentStage.choices, blank=True, null=True
+        max_length=21,
+        choices=DevelopmentStage.choices,
+        default=DevelopmentStage.PREALPHA,
     )
-    version = VersionField()
+    version = VersionField(default="0.0.1")
     banner_message = models.TextField(blank=True, null=True)
 
     # Ownership Info
     author = models.CharField(max_length=50)
     author_email = models.EmailField(blank=True, null=True)
+    author_url = models.URLField(blank=True, null=True)
     repository_url = models.URLField()
     homepage_url = models.URLField(blank=True, null=True)
     support_url = models.URLField(blank=True, null=True)
     donation_url = models.URLField(blank=True, null=True)
     pypi_url = models.URLField(blank=True, null=True)
-    license_type = models.CharField(max_length=100, default="GPLv3")
-
-    # Environment
-    sys_platforms = MultiSelectField(choices=SysPlatforms.choices, max_length=10)
+    license_type = models.CharField(
+        max_length=20,
+        choices=LicenseTypes.choices,
+        default=LicenseTypes.GPL3,
+    )
 
     # Compatibility
+    sys_platforms = MultiSelectField(choices=SysPlatforms.choices, max_length=10)
+    desktop_compatible = models.BooleanField()
     touch_compatible = models.BooleanField()
     mobile_compatible = models.BooleanField()
-    minimum_conreq_version = VersionField()
-    tested_conreq_version = VersionField()
+    minimum_conreq_version = VersionField(default="0.0.1")
+    tested_conreq_version = VersionField(default="1.0.0")
     max_conreq_version = VersionField(blank=True, null=True)
-    asynchronous = models.CharField(max_length=20, choices=AsyncCompatibility.choices)
+    asynchronous = models.CharField(
+        max_length=20,
+        choices=AsyncCompatibility.choices,
+        default=AsyncCompatibility.NONE,
+    )
 
     # App Dependencies
-    required_apps = models.ManyToManyField("self", blank=True)
-    optional_apps = models.ManyToManyField("self", blank=True)
-    incompatible_apps = models.ManyToManyField("self", blank=True)
+    required_app_packages = models.ManyToManyField("self", blank=True)
+    optional_app_packages = models.ManyToManyField("self", blank=True)
+    incompatible_app_packages = models.ManyToManyField("self", blank=True)
     incompatible_categories = models.ManyToManyField(
         Category, related_name="incompatible_categories", blank=True
     )
@@ -131,13 +159,13 @@ class AppPackage(models.Model):
 
 class EnvironmentVariable(models.Model):
     def __str__(self):
-        return self.name + ' = "' + str(self.default) + '"'
+        return self.name + ' = "' + str(self.default_value) + '"'
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
-    default = models.CharField(max_length=255, blank=True, null=True)
-    example = models.CharField(max_length=255, blank=True, null=True)
-    required = models.BooleanField(default=False, blank=True, null=True)
+    required = models.BooleanField(default=False)
+    default_value = models.CharField(max_length=255, blank=True, null=True)
+    example_values = models.CharField(max_length=255, blank=True, null=True)
     app_package = models.ForeignKey(AppPackage, on_delete=models.CASCADE)
 
 
@@ -158,6 +186,5 @@ class NoticeMessage(models.Model):
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    mark_read = models.BooleanField(default=False, blank=True, null=True)
+    message = models.TextField()
     app_package = models.ForeignKey(AppPackage, on_delete=models.CASCADE)
