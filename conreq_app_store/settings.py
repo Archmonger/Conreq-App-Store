f3d4cacee85e7bed48140ecd32d07dafc8f462f9
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-10 14:07:34 -0700

    add bootstrap admin

--- conreq_app_store/settings.py
+++ conreq_app_store/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
+    "bootstrap_admin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
